efb54e4101bdf388054b585d144ebbc62c07dd85
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-24 20:06:25 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+// let submitData = {
+//     function (name,email){
+//          fetch("http://localhost:3000/users",{
+//         method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     name ,
+//     email,
+//   }),
+//     })
+//     .then( function ( response ) {
+//         return response.json()
+//       } )
+//       .then( function ( object ) {
+//         document.body.innerHTML = object[ "id" ]
+//       } )
+// }
+//     }
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name ,
+    email,
+  }),
+    })
+    .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+       document.body.innerHTML = object[ "id" ]
+        // document.getelementbyid(id).innerhtml='<object>'
+        // console.log(object);
+      } )
+       .catch( function ( error ) {
+        document.body.innerHTML = error.message
+    } )
+} 
